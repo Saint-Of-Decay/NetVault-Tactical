390bc74d2a05301c7f6ad67dd04ee2e8adc7473c
Saint-Of-Decay <williamwfuller47@gmail.com>
2026-06-16 19:03:38 -0500

    Create site.js
    Needed to build the site config.

---
+++ assets/site.js
@@ -0,0 +1,13 @@
+function toggleNav() {
+  document.getElementById('sidenav').classList.toggle('open');
+}
+
+function openModal(id) {
+  const el = document.getElementById(id);
+  if (el) el.classList.add('show');
+}
+
+function closeModal(id) {
+  const el = document.getElementById(id);
+  if (el) el.classList.remove('show');
+}
